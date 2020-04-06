ce94f3cca86cce8617d934db018a43500ff642a0
BromvliegZwaan <olehouwing@ziggo.nl>
2020-04-06 10:33:18 +0000

    Jo

--- public/PHP/opdracht_2.php
+++ public/PHP/opdracht_2.php
@@ -7,11 +7,14 @@ TYP HIERONDER JOUW PHPCODE
 
 $tekst="Ik heb een bijbaantje bij de Aldi. De Aldi betaalt goed.";
 echo "$tekst";
-//$tekst=str_replace('Aldi','Albert Heijn',$tekst);
-echo '<h4>$tekst</h4>';
-
+$tekst=str_replace('Aldi','Albert Heijn',$tekst);
+$tekst=strstr($tekst,"De");
+echo  $tekst;
+$lengte=strlen($tekst);
+echo '<i>De string $tekst bestaat uit <b>'.$lengte.' </b>karakters.</i><br>';
+$tekst=substr($tekst,10,5);
 /****************************
 EINDE VAN JOUW PHPCODE
 ****************************/
 include('opdracht_eind.php');
-?>
+ 
